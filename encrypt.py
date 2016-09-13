8306d55be984fff72d0ee3a4d66e6ee50b34f360
Scratch45 <evancb45@gmail.com>
2016-09-12 18:29:21 -0700

    Added a main function
    The main function allows encrypt.py to have mutiple functions that are
    now called instead of instant responses

--- encrypt.py
+++ encrypt.py
@@ -1,14 +1,20 @@
 #Created by Scratch45
 #Imports
 import hashlib
+inputText = " "
+plainText = " "
 
-#Creates a new hashlib object using DSA encryption
-m = hashlib.new('DSA')
+#Main class, encrypts text that is inputed
+def main():
+    m = hashlib.new('DSA')
 
 #Asks the user to input the text he or she wants to be encrypted and encodes in utf-8
-inputtext = input('Enter text to be encrypted: ')
-plaintext = inputtext.encode('utf-8')
+    inputText = input('Enter text to be encrypted: ')
+    plaintext = inputText.encode('utf-8')
 
 #sends the response to m the hashlib object and prints it
-m.update(plaintext)
-print("Here is your encrypted message: ",m.hexdigest())
+    m.update(plaintext)
+    print("Here is your encrypted message: ",m.hexdigest())
+
+
+
