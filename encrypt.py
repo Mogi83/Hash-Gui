a0fe86b0e35ea818efea34261bbc0380c24f4faa
Scratch45 <evancb45@gmail.com>
2016-09-12 18:00:14 -0700

    Main encryption
    Added functionality to have the user input text and added commentation

--- encrypt.py
+++ encrypt.py
@@ -1,7 +1,14 @@
+#Created by Scratch45
+#Imports
 import hashlib
 
+#Creates a new hashlib object using DSA encryption
 m = hashlib.new('DSA')
-plaintext = "text".encode('utf-8')
 
+#Asks the user to input the text he or she wants to be encrypted and encodes in utf-8
+inputtext = input('Enter text to be encrypted: ')
+plaintext = inputtext.encode('utf-8')
+
+#sends the response to m the hashlib object and prints it
 m.update(plaintext)
-print(m.hexdigest())
+print("Here is your encrypted message: ",m.hexdigest())
